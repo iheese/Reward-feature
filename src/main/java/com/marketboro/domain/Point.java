7209875ddba49133e080e195ae95a31aab12e7e4
iheese <imbc0224@gmail.com>
2023-08-11 22:38:30 +0900

    feat: Point 업데이트

--- src/main/java/com/marketboro/domain/Point.java
+++ src/main/java/com/marketboro/domain/Point.java
@@ -1,29 +1,50 @@
 package com.marketboro.domain;
 
+import com.marketboro.domain.common.BaseTime;
+import com.marketboro.domain.enums.PointStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
 @Entity
-public class Point {
+public class Point extends BaseTime {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "point_no")
     private Long pointNo;
     @Column(name = "point_value")
     private Long pointValue;
-    @Column(name = "reg_date")
-    private LocalDateTime regDate;
     @Column(name = "exp_date")
     private LocalDateTime expDate;
-    @Column(name = "use_date")
-    private LocalDateTime useDate;
-    @ManyToOne
+    @Column(name = "point_status")
+    private PointStatus pointStatus;
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_no")
     private User user;
+    @Builder.Default
+    @OneToMany(mappedBy = "point")
+    private List<PointDetail> pointDetailList = new ArrayList<>();
+
+    @PrePersist
+    public void setExpDate() {
+        this.expDate = getRegDate().plusYears(1);
+    }
 }
