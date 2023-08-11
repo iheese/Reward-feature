9b00ad557cb2d972e0963e9dda7b61b8e37687b5
iheese <imbc0224@gmail.com>
2023-08-11 22:41:06 +0900

    feat: UserService 추가

---
+++ src/main/java/com/marketboro/service/UserService.java
@@ -0,0 +1,25 @@
+package com.marketboro.service;
+
+import com.marketboro.common.exception.CustomException;
+import com.marketboro.common.exception.ErrorCode;
+import com.marketboro.domain.User;
+import com.marketboro.dto.UserResponse;
+import com.marketboro.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional(readOnly=true)
+    public UserResponse.UserPointAmount getUserPoint(Long userNo) {
+        return new UserResponse.UserPointAmount(getUser(userNo));
+    }
+
+    public User getUser(Long userNo) {
+        return userRepository.findById(userNo)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+    }
+}
