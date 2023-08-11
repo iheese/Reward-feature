aaeb5c45c4378faa1762ca8565dfcfc5d2c9bfc2
iheese <imbc0224@gmail.com>
2023-08-11 22:41:24 +0900

    feat: User 메소드 추가

--- src/main/java/com/marketboro/domain/User.java
+++ src/main/java/com/marketboro/domain/User.java
@@ -1,28 +1,40 @@
 package com.marketboro.domain;
 
+import com.marketboro.domain.common.BaseTime;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
 import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Entity
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseTime {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_no")
     private Long userNo;
     @Column(name = "userName")
     private String userName;
-    @Column(name = "reg_date")
-    private LocalDateTime regDate;
     @Column(name = "point_amount")
     private Long pointAmount;
     @OneToMany(mappedBy = "user")
     private List<Point> pointHistory = new ArrayList<>();
+
+    public User addPoint(Long point) {
+        this.pointAmount += point;
+        return this;
+    }
+
+    public User minusPoint(Long point) {
+        this.pointAmount -= point;
+        return this;
+    }
 }
