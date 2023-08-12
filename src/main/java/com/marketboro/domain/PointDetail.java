33760d76479f9717e1e56fb1da5e26d99d52c073
iheese <imbc0224@gmail.com>
2023-08-12 13:53:22 +0900

    config : PointDetail 수정

--- src/main/java/com/marketboro/domain/PointDetail.java
+++ src/main/java/com/marketboro/domain/PointDetail.java
@@ -4,6 +4,8 @@ import com.marketboro.domain.enums.PointDetailStatus;
 import com.marketboro.domain.enums.PointStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,24 +25,35 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 public class PointDetail {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "point_detail_no")
     private Long pointDetailNo;
+
     @Column(name = "point_detail_value")
     private Long pointDetailValue;
+
     @Column(name = "exp_date")
     private LocalDateTime expDate;
+
     @Column(name = "point_status")
+    @Enumerated(EnumType.STRING)
     private PointStatus pointStatus;
+
+    @Enumerated(EnumType.STRING)
     @Column(name = "point_detail_status")
     private PointDetailStatus pointDetailStatus;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "point_no")
     private Point point;
 
-    public PointDetail minusPoint(Long point) {
-        this.pointDetailValue -= point;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private User user;
+
+    public PointDetail updatePointDetailStatus() {
+        this.pointDetailStatus = PointDetailStatus.USED;
+        this.expDate = null;
         return this;
     }
 }
