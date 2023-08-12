b349d3de110d7a0420dc3e6d05f62502ea30a1f8
iheese <imbc0224@gmail.com>
2023-08-12 13:54:25 +0900

    config : PointResponse 필드 추가

--- src/main/java/com/marketboro/dto/PointResponse.java
+++ src/main/java/com/marketboro/dto/PointResponse.java
@@ -1,22 +1,24 @@
 package com.marketboro.dto;
 
 import com.marketboro.domain.Point;
+import com.marketboro.domain.enums.PointStatus;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
 public class PointResponse {
+    @Getter
     public static class PointHistory {
         private Long pointNo;
         private Long pointValue;
+        private PointStatus pointStatus;
         private LocalDateTime regDate;
         private LocalDateTime expDate;
-        private LocalDateTime useDate;
 
         public PointHistory(Point point) {
             this.pointNo = point.getPointNo();
             this.pointValue = point.getPointValue();
+            this.pointStatus = point.getPointStatus();
             this.regDate = point.getRegDate();
             this.expDate = point.getExpDate();
         }
