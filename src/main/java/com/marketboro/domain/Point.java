0dfee6959323b5014fea0bea0fc25d185be8580a
iheese <imbc0224@gmail.com>
2023-08-12 13:48:43 +0900

    config : Point 이넘 에러 수정

--- src/main/java/com/marketboro/domain/Point.java
+++ src/main/java/com/marketboro/domain/Point.java
@@ -4,6 +4,8 @@ import com.marketboro.domain.common.BaseTime;
 import com.marketboro.domain.enums.PointStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,15 +32,21 @@ public class Point extends BaseTime {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "point_no")
     private Long pointNo;
+
     @Column(name = "point_value")
     private Long pointValue;
+
     @Column(name = "exp_date")
     private LocalDateTime expDate;
+
     @Column(name = "point_status")
+    @Enumerated(EnumType.STRING)
     private PointStatus pointStatus;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_no")
     private User user;
+
     @Builder.Default
     @OneToMany(mappedBy = "point")
     private List<PointDetail> pointDetailList = new ArrayList<>();
