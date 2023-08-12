c4fe5511e9492d4c0611ae985ffd619d7aced9c6
iheese <imbc0224@gmail.com>
2023-08-12 13:47:58 +0900

    config : ErrorCode 수정

--- src/main/java/com/marketboro/common/exception/ErrorCode.java
+++ src/main/java/com/marketboro/common/exception/ErrorCode.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    NOT_FOUND_USER(1000, "주어진 id 값의 회원을 조회할 수 없습니다."),
-    NOT_NUMBER(1001, "주어진 id 값이 올바르지 않습니다."),
-    NOT_ENOUGH_POINT(1002, "사용하시려는 포인트가 소유한 포인트보다 많습니다.");
+    NOT_FOUND_USER(100, "주어진 id 값의 회원을 조회할 수 없습니다."),
+    NOT_NUMBER(101, "주어진 id 값이 올바르지 않습니다."),
+    NOT_ENOUGH_POINT(102, "사용하시려는 포인트가 소유한 포인트보다 많습니다.");
 
     private final Integer statusCode;
     private final String message;
