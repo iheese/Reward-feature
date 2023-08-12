3ec75bb88014d21fe040b7e6e4f4a408b1f0416a
iheese <imbc0224@gmail.com>
2023-08-12 14:52:34 +0900

    feat : PointRepository 수정

--- src/main/java/com/marketboro/repository/PointRepository.java
+++ src/main/java/com/marketboro/repository/PointRepository.java
@@ -6,12 +6,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface PointRepository extends JpaRepository<Point, Long> {
-    Page<Point> findByUserOrderByIdDesc(User user, Pageable pageable);
-
-    List<Point> findByUserOrderByIdDesc(User user);
-
-    Page<Point> findByUser_UserNoOrderByIdDesc(Long userNo, Pageable pageable);
+    Page<Point> findByUserOrderByPointNoDesc(User user, Pageable pageable);
 }
