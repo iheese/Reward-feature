44b5935a6358c77435d49d792d6dfc99a4b0e0aa
iheese <imbc0224@gmail.com>
2023-08-12 13:52:37 +0900

    config : PointController 수정

--- src/main/java/com/marketboro/controller/PointController.java
+++ src/main/java/com/marketboro/controller/PointController.java
@@ -10,27 +10,29 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequestMapping("/point")
 @RequiredArgsConstructor
 @RestController
 public class PointController {
     private final PointService pointService;
 
-    @PostMapping("/point/getPointHistory")
+    @PostMapping("/getPointHistory")
     public ResponseEntity<List<PointResponse.PointHistory>> getPointHistory(@Valid @RequestBody PointRequest.PointHistory request) {
         return new ResponseEntity(pointService.getPointHistory(request), HttpStatus.OK);
     }
 
-    @PostMapping("/point/rewardPoint")
+    @PostMapping("/rewardPoint")
     public ResponseEntity<UserResponse.UserPointAmount> rewardPoint(@Valid @RequestBody PointRequest.PointReward request) {
         return new ResponseEntity(pointService.rewardPoint(request), HttpStatus.OK);
     }
 
-    @PostMapping("/point/usePoint")
+    @PostMapping("/usePoint")
     public ResponseEntity<UserResponse.UserPointAmount> usePoint(@Valid @RequestBody PointRequest.PointUsage request) {
-        return  new ResponseEntity(pointService.usePoint(request), HttpStatus.OK);
+        return new ResponseEntity(pointService.usePoint(request), HttpStatus.OK);
     }
 }
