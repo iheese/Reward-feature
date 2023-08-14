0391510b122cc73a88e75e033eea7b87b488422b
iheese <imbc0224@gmail.com>
2023-08-14 16:25:22 +0900

    feat : User 생성자, 빌더 패턴 추가

--- src/main/java/com/marketboro/domain/User.java
+++ src/main/java/com/marketboro/domain/User.java
@@ -7,16 +7,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTime {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_no")
