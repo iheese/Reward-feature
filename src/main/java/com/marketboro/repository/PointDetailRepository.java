52ab502f0085dbcea76c393bc18bc7cf5b9c8885
iheese <imbc0224@gmail.com>
2023-08-14 16:26:09 +0900

    feat : PointDetailRepository 라인정리

--- src/main/java/com/marketboro/repository/PointDetailRepository.java
+++ src/main/java/com/marketboro/repository/PointDetailRepository.java
@@ -8,6 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PointDetailRepository extends JpaRepository<PointDetail, Long> {
-
     List<PointDetail> findByUserAndPointDetailStatusOrderByExpDateAsc(User user, PointDetailStatus pointDetailStatus);
 }
