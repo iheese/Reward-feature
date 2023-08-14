b04c480442a09a0e114f5704a10db0fa06c96baa
iheese <imbc0224@gmail.com>
2023-08-14 16:25:42 +0900

    feat : UserRequest 생성자, 빌더 패턴 추가

--- src/main/java/com/marketboro/dto/UserRequest.java
+++ src/main/java/com/marketboro/dto/UserRequest.java
@@ -1,8 +1,14 @@
 package com.marketboro.dto;
 
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 public class UserRequest {
     @Positive
